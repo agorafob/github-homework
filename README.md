b3e78d06b021ace59ae4c972774244c380ab86af
agorafob <alexander.ptrsyn@gmail.com>
2021-03-05 11:27:54 +0200

    [HW-02] Second commit for branch

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ First line for commit
 Second line for commit
 Third line for commit
 First Line for branch 
+Second line for branch 
--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static void print (String line){
+        System.out.println(line);
+    }
 }
